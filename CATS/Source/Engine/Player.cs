f7db283f0acc9ff3b7fcddcdf71a47c5a577bfde
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2021-08-26 13:09:34 -0600

    Player movement

---
+++ CATS/Source/Engine/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace KATSS
+{
+    public class Player : Item2D
+    {
+        public Keys _left;
+        public Keys _right;
+        public int move = 5;
+
+        public Player(string PATH, Vector2 pos, Vector2 dimension, Keys left, Keys right) : base(PATH, pos, dimension)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override void Update()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(_left))
+                _pos.X -= move;
+            if (state.IsKeyDown(_right))
+                _pos.X += move;
+
+        }
+        
+    }
+}
--- CATS/Source/World.cs
+++ CATS/Source/World.cs
@@ -2,26 +2,30 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace KATSS
 {
     public class World
     {
         public Item2D bg;
+        public Player player1;
 
         public World()
         {
             bg = new Item2D("Images\\firstLook", new Vector2(0,0), new Vector2(1920, 1080));
+            player1 = new Player("character_wire_frame", new Vector2(300, 800), new Vector2(106,202), Keys.Left, Keys.Right);
         }
 
         public void Update()
         {
-
+            player1.Update();
         }
 
         public void Draw()
         {
             bg.Draw();
+            player1.Draw();
         }
     }
 }
