dc156a1b054ffb2b8364d9b20ed2f1a3f8ad4913
KD1695 <kirtidhwajsingh95@gmail.com>
2021-08-25 20:07:38 -0600

    Added firstLook display in full screen size

---
+++ CATS/Main.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace KATSS
+{
+    public class Main : Game
+    {
+        World world;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1920;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = 1080;   // set this value to the desired height of your window
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Globals.content = this.Content;
+            Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            world = new World();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            world.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            Globals.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            world.Draw();
+
+
+            Globals.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            using (var game = new Main())
+                game.Run();
+        }
+    }
+}
---
+++ CATS/Source/World.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATSS
+{
+    public class World
+    {
+        public DrawBg bg;
+
+        public World()
+        {
+            bg = new DrawBg("Images\\firstLook", new Vector2(0,0), new Vector2(1920, 1080));
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            bg.Draw();
+        }
+    }
+}
