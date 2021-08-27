9188e4b34f047f5e4965b1743b115fd80b60eabb
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2021-08-27 15:07:32 -0600

    2 Player move left and right

--- CATS/Source/Engine/Player.cs
+++ CATS/Source/Engine/Player.cs
@@ -12,22 +12,34 @@ namespace KATSS
         public Keys _left;
         public Keys _right;
         public int move = 5;
+        float leftBound;
+        float rightBound;
 
         public Player(string PATH, Vector2 pos, Vector2 dimension, Keys left, Keys right) : base(PATH, pos, dimension)
         {
             _left = left;
             _right = right;
+            //_isPlayer1 = isPlayer1;
+            leftBound = _pos.X - 480;
+            rightBound = _pos.X + 480;
         }
 
         public override void Update()
         {
+            
             KeyboardState state = Keyboard.GetState();
 
             if (state.IsKeyDown(_left))
+            {
                 _pos.X -= move;
-            if (state.IsKeyDown(_right))
-                _pos.X += move;
+            }
 
+            if (state.IsKeyDown(_right))
+            {
+                _pos.X += move;
+            }
+
+            _pos.X = MathHelper.Clamp(_pos.X, leftBound, rightBound);
         }
         
     }
